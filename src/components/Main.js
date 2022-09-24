201b69d05c43e2e5a989fab5822cdd76011652bd
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-09-24 19:54:33 -0400

    imports and defines clientsided routes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,16 @@
+import Index from '../pages/Index';
+import Show from '../pages/Show';
+import { Routes, Route } from 'react-router-dom';
 // eslint-disable-next-line no-unused-vars
 function Main(props) {
-  return <h2>This is the main content</h2>;
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/experiences/:id" element={<Show />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default Main;
