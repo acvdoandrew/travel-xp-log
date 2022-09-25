91713e63ad9e2802481e5220f3f4978bc59b5b2d
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-09-24 20:13:32 -0400

    makes logo in navbar a link for homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
+import { Link } from 'react-router-dom';
 // eslint-disable-next-line no-unused-vars
 function Header(props) {
   return (
     <nav className="nav">
-      <div>Logo</div>
+      <Link to="/">
+        <div>Logo</div>
+      </Link>
       <div>Welcome, Username!</div>
     </nav>
   );
