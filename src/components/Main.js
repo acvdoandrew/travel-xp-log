22994823a5fa481e8fd072656b0e9d79b6605a61
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-09-27 12:41:07 -0400

    succesfully calls API from backend

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,33 @@
+/* eslint-disable no-unused-vars */
+import { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Index from '../pages/Index';
 import Show from '../pages/Show';
-import { Routes, Route } from 'react-router-dom';
 // eslint-disable-next-line no-unused-vars
 function Main(props) {
+  const [travel, setTravel] = useState(null);
+
+  const API_URL = 'http://localhost:4000/api/travel/';
+
+  const getData = async () => {
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      setTravel(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <main>
       <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/experiences/:id" element={<Show />} />
+        <Route path="/" element={<Index travel={travel} />} />
+        <Route path="/travel/:id" element={<Show />} />
       </Routes>
     </main>
   );
