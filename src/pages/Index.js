9e50535034494e551db64c2a8a76cb4c914e1638
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-09-27 13:07:34 -0400

    displays data in index page

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,6 +1,26 @@
-// eslint-disable-next-line no-unused-vars
-function Index(props) {
-  return <h2>This is the Index Page!</h2>;
+/* eslint-disable no-unused-vars */
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+// eslint-disable-next-line react/prop-types
+function Index({ travel }) {
+  const loaded = () => {
+    return travel.map((t) => (
+      <div className="travel" key={t._id}>
+        <h3>{t.location}</h3>
+        <div className="img-wrapper">
+          <img src={t.image} alt={t.location} />
+        </div>
+        <h4>{t.cost}</h4>
+      </div>
+    ));
+  };
+
+  const loading = () => {
+    return <h1>Loading...</h1>;
+  };
+
+  return travel ? loaded() : loading();
 }
 
 export default Index;
