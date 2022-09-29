19b20b17dd9420c350fd58a2c5250b851d747151
zeuselijah <simeonhendricks@gmail.com>
2022-09-29 10:17:10 -0400

    wip:set up comment components and sets up in main.js

---
+++ src/components/Comment.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+//import { useState, useEffect } from 'react';
+// import { Routes, Route } from 'react-router-dom';
+
+function Comment(comment) {
+  return (
+    <section>
+      <h5>Comment</h5>
+      <form>
+        <input type="text" />
+      </form>
+    </section>
+  );
+}
+
+export default Comment;
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,11 +3,15 @@ import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Index from '../pages/Index';
 import Show from '../pages/Show';
+import Comment from './Comment';
+
 // eslint-disable-next-line no-unused-vars
 function Main(props) {
   const [travel, setTravel] = useState(null);
+  const [comment, setComment] = useState(null);
 
   const API_URL = 'http://localhost:4000/api/travel/';
+  const API_URL2 = 'http://localhost:4000/api/travel/comment';
 
   const getData = async () => {
     try {
@@ -60,8 +64,34 @@ function Main(props) {
     }
   };
 
+  const getCommentData = async () => {
+    try {
+      const response = await fetch(API_URL2);
+      const data = await response.json();
+      setComment(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const createComment = async (comment) => {
+    try {
+      await fetch(API_URL2, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'Application/json',
+        },
+        body: JSON.stringify(comment),
+      });
+      getCommentData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     getData();
+    getCommentData;
   }, []);
 
   return (
@@ -74,11 +104,14 @@ function Main(props) {
         <Route
           path="/travel/:id"
           element={
-            <Show
-              travel={travel}
-              deleteTravel={deleteTravel}
-              updateTravel={updateTravel}
-            />
+            <>
+              <Show
+                travel={travel}
+                deleteTravel={deleteTravel}
+                updateTravel={updateTravel}
+              />
+              <Comment comment={comment} submit={createComment} />
+            </>
           }
         />
       </Routes>
